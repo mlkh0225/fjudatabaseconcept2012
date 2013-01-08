821008cdc65b9bbd0710bd406f9ad49338d38d66
風仔™ <mlkh0225@auth9-112.auth.fju.edu.tw>
2013-01-08 13:01:56 +0800

    Page 1 SQL 刪除重複

--- SQL/Page1.sql
+++ SQL/Page1.sql
@@ -6,40 +6,20 @@ where
 		select A.train_number
 		from train_stop_at as S
 		where
-			exists((
-				select distinct seat_ID
-				from seat)
-				except(
-					select distinct T.seat_ID
-					from reserved_list as T
-					where train_date='乘車日期'
-						and S.train_number = T.train_number)))
-		and A.station_ID=5--客戶選擇的啟程站 ex.3
-		and B.station_ID=8
-		and A.depart_time>=360--客戶選擇的啟程時間 ex.360
-		and A.train_number in(
-			select A.train_number
-			from train
-			where start_station<end_station)--南下北上 ex.南下（<）北上（>）
-		--車次有訂位但搭乘區間有空位
-		or A.train_number in(
-			select A.train_number
-			from train_stop_at as S
-			where
-			exists((
-				select distinct seat_ID
-				from seat)
-				except(
-					select seat_ID
-					from reserved_list as T
-					where S.train_number=T.train_number
+		exists((
+			select distinct seat_ID
+			from seat)
+			except(
+				select seat_ID
+				from reserved_list as T
+				where S.train_number=T.train_number
 					and A.station_ID between 5 and 7--已經有訂位但在客戶搭乘「區間（最後一站減一）」有空位
 					and train_date='乘車日期')))
-		and A.station_ID=5--客戶選擇的啟程站
-		and B.station_ID=8
-		and A.depart_time>=360--客戶選擇的啟程時間 ex.360
-		and A.train_number in(
-			select A.train_number
-			from train
-			where start_station<end_station)--南下北上 ex.南下（<）北上（>）
+	and A.station_ID=5--客戶選擇的啟程站
+	and B.station_ID=8
+	and A.depart_time>=360--客戶選擇的啟程時間 ex.360
+	and A.train_number in(
+		select A.train_number
+		from train
+		where start_station<end_station)--南下北上 ex.南下（<）北上（>）
 	order by A.depart_time asc;--以出發時間排序
